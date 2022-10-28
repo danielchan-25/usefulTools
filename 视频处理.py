7d98e71ad50444c884b94a70b7416d352b525e05
daniel_chan <295469301@qq.com>
2022-10-28 18:34:57 +0800

    update

--- 视频处理.py
+++ 视频处理.py
@@ -1,5 +1,6 @@
 import os,time,ffmpy3
-
+from cv2 import split
+from re import U
 
 def check(video_path):
     if os.path.exists(video_path + 'title.mp4'):
@@ -24,22 +25,19 @@ def check(video_path):
         os.mkdir(video_path + 'done_video/')
 
 def make_title(video_path):
+    # 片头视频URL地址
     title_video_path = video_path + 'title.mp4'
     title_video_translate_path = video_path + 'title.ts'
+    # 转码为 title.ts
     transcode_title = 'ffmpeg -i\t' + title_video_path + '\t'+ title_video_translate_path
     os.system(transcode_title)
 
 def add_pink(video_path):
+    # 视频URL地址
     tmp_video = input_path + 'tmp_video/'
     video_path = video_path + 'video/'
     video_list = os.listdir(video_path)
-    #print('需要处理的视频有：' + str(video_list))
-    #user_confirm = input('确认无误输入：y/Y，退出则输入：n/N：')
-    #if user_confirm == 'y' or user_confirm == 'Y':
-    #    print('开始转码')
-    #else:
-    #    print('已停止')
-    #    os._exit()
+    # 添加粉边，并转码为ts文件
     for i in video_list:
         video_url = video_path + i
         transcode_add_pink = 'ffmpeg -i\t' + video_url + "\t-vf 'scale=1408:1056,pad=1920:1080:256:12:#fcccd4'\t" + tmp_video + i + '.ts'
@@ -47,6 +45,7 @@ def add_pink(video_path):
         os.system(transcode_add_pink)
 
 def merge_video(video_path):
+    # 合并两个ts文件
     title_video_path = input_path + 'title.ts'
     tmp_path = os.listdir(input_path + 'tmp_video/')
     done_path = input_path + 'done_video/'
