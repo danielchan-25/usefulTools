6c568e761d6b30f64b7c344255726e62319b771b
daniel_chan <295469301@qq.com>
2022-11-06 14:48:20 +0800

    update

---
+++ 猜你姓氏.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+import tkinter
+
+
+
+# 全局设置
+window = Tk()
+window.geometry('400x300+300+100')
+window.config(background="#FFFFFF")
+
+
+
+name = input('请输入您的姓氏：')
