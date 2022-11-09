a5b1bd7facf25def701c5922f8c2d88aee8d4fc1
daniel_chan <295469301@qq.com>
2022-11-09 17:43:25 +0800

    update

--- 我来帮你说.py
+++ 我来帮你说.py
@@ -1,3 +1,10 @@
-from tkinter import *
-import tkinter
+import requests
 
+word = input('输入需要我帮你说的事：')
+url = "https://www.jiuwa.net/tools/jupai/index.php"
+resp = requests.post(url,data={'t':word})
+photo_url = f'https://www.jiuwa.net/{resp.content.decode("utf-8")}'
+
+with open('upuptoyou.png',mode='wb') as f:
+    f.write(requests.get(photo_url).content)
+print('图片生成完成。')
